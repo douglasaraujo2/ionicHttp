36eb2821966bdf07938ebc5397c52bc7d85cc260
logonrm <logonrm@fiap.com.br>
2017-11-25 15:01:52 -0200

    All files

---
+++ src/pages/detalhes/detalhes.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DetalhesPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-detalhes',
+  templateUrl: 'detalhes.html',
+})
+export class DetalhesPage {
+  email: string
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.email = navParams.get('email');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DetalhesPage');
+  }
+
+}
